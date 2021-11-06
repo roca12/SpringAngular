a14762c7256a809b3776d42525b01dead349371d
Diego Rodriguez <diegof.rodriguezc@ecci.edu.co>
2021-11-06 09:31:17 -0500

    login g27

--- SpringAngularFront/Grupo27/src/app/app.component.ts
+++ SpringAngularFront/Grupo27/src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Grupo27';
+  title = 'Login';
+
+  listausuarios=["diego","sergio","admin","user"];
+  listapass=["diego","sergio","admin","user"];
+
+
+  user_correcto: string = "admininicial";
+  pass_correcto: string = "admin123456";
+
+  user: string = "";
+  pass: string = "";
+
+  correcto: number=-1;
+  comparar() {
+    if (this.user === this.user_correcto) {
+      this.correcto = 1;
+      if (this.pass === this.pass_correcto) {
+        this.correcto = 1;
+      } else {
+        this.correcto = 0;
+      }
+    } else {
+      this.correcto = 0;
+    }
+
+  }
 }
